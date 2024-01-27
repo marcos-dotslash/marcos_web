d454e9336bc3e9b9e8e59c1fc907becabdec0dd9
Raj Pansara <rajpansara910@gmail.com>
2024-01-27 12:40:24 +0530

    create compponent

---
+++ pages/api/crud/create_componet.ts
@@ -0,0 +1,36 @@
+import { connectDB } from '@/utils/dbconnect';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import Componets from "@/models/componets";
+import { getSession } from 'next-auth/react';
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if(req.method === 'POST') {
+         try{
+            const session = await getSession({ req });
+
+            if (!session) {
+                return res.status(401).json({ error: 'Unauthorized' });
+            }
+            await connectDB();
+            const { text } = req.body;
+            const comp = new Componets();
+            comp.html = text.html;
+            comp.css = text.css;
+            comp.js = text.js;
+            comp.category = text.category;
+            comp.user = session.user.id;
+            comp.save();
+            res.status(200).json({ text: 'COMPONENT ADDED SUCCSEFULLY?!!!' });
+        }catch (e) {
+            console.error(e);
+            res.status(500).json({ error: "Internal Server Error" });
+          }
+    }
+
+    else {
+        res.status(200).json({ text: 'Bye' });
+        console.log("helloa");
+
+    }
+}
