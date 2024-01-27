762edafd1d87990a69df5b1f11c37cdecd4f6d1a
Raj Pansara <rajpansara910@gmail.com>
2024-01-27 14:09:42 +0530

    read componet

--- pages/api/crud/read_componet.ts
+++ pages/api/crud/read_componet.ts
@@ -7,23 +7,11 @@ import { getSession } from 'next-auth/react';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method === 'POST') {
          try{
-            const session = await getSession({ req });
-
-            if (!session) {
-                return res.status(401).json({ error: 'Unauthorized' });
-            }
             await connectDB();
             const text = req.body;
-            console.log("1");
-            const comp = new Componets();
-            comp.html = text.html;
-            comp.css = text.css;
-            comp.js = text.js;
-            comp.category = text.category;
+            const comp = await Componets.find({category : text.category});
 
-            comp.user = session.user.id;
-            comp.save();
-            res.status(200).json({ text: 'COMPONENT ADDED SUCCSEFULLY?!!!' });
+            res.status(200).json({ componet: comp });
         }catch (e) {
             console.error(e);
             res.status(500).json({ error: "Internal Server Error" });
