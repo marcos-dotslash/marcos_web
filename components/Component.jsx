2ccc02e55fcf710627d65612557e7a724854dced
Hitarth <patelhitarth08@gmail.com>
2024-01-27 22:56:14 +0530

    modified UI

--- components/Component.jsx
+++ components/Component.jsx
@@ -2,120 +2,189 @@ import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 import Editor from "./codeEditor/Editor";
 import Button from "./codeEditor/Button";
+import Draggable from "react-draggable";
 
 const Component = (props) => {
-  const {components}  = props
+  const { components, changeComponents } = props;
 
-  useEffect(()=>{
-    // console.log("c "+components[0].html)
-  },[components])
+  // const [code, setCode] = useState(components);
 
-  // const { components } = props;
-  const [indexComponent , setIndexComponent] = useState(-1);
-  const [html, setHtml] = useState('');
-  const [css, setCss] = useState('');
-  const [js, setJs] = useState('');
-  // const [code, setCode] = useState("");
+  const [indexComponent, setIndexComponent] = useState(-1);
+  const [html, setHtml] = useState("");
+  const [css, setCss] = useState("");
+  const [js, setJs] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(true);
-  const [openedEditor, setOpenedEditor] = useState('html');
+  const [openedEditor, setOpenedEditor] = useState("html");
+  const [show, setShow] = useState("hidden");
 
   const onTabClick = (editorName) => {
     setOpenedEditor(editorName);
   };
 
-  const setHtmlCode = (value)=>{
-     setHtml((prev)=>{
-       props.changeCodes(value,css,js,indexComponent)
-      return value
-    })
-  }
+  const setHtmlCode = (value) => {
+    setHtml((prev) => {
+      props.changeCodes(value, css, js, indexComponent);
+      return value;
+    });
+  };
 
-  const setCssCode = (value)=>{
-    setCss((prev)=>{
-      props.changeCodes(html,value,js,indexComponent)
-      return value
-    })
-  }
+  const setCssCode = (value) => {
+    setCss((prev) => {
+      props.changeCodes(html, value, js, indexComponent);
+      return value;
+    });
+  };
 
-  const setJsCode = (value)=>{
-    setJs((prev)=>{
-      props.changeCodes(html,css,value,indexComponent)
-      return value
-    })
-  }
+  const setJsCode = (value) => {
+    setJs((prev) => {
+      props.changeCodes(html, css, value, indexComponent);
+      return value;
+    });
+  };
+  const moveElementUp = (index) => {
+    if (index > 0) {
+      const updatedComponents = [...components];
+      [updatedComponents[index - 1], updatedComponents[index]] = [
+        updatedComponents[index],
+        updatedComponents[index - 1],
+      ];
+      changeComponents(updatedComponents);
+    }
+  };
 
+  const moveElementDown = (index) => {
+    if (index < components.length - 1) {
+      const updatedComponents = [...components];
+      [updatedComponents[index], updatedComponents[index + 1]] = [
+        updatedComponents[index + 1],
+        updatedComponents[index],
+      ];
+      changeComponents(updatedComponents);
+    }
+  };
+
+  const deleteElement = (index) => {
+    const updatedComponents = [...components];
+    updatedComponents.splice(index, 1);
+    changeComponents(updatedComponents);
+  };
 
   return (
-    <div>    
+    <div className="container mx-auto px-16 my-10">
+      {/* <div className="grid grid-cols-1 mx-10 md:grid-cols-2 gap-10 justify-center items-center"> */}
+      <div>
         {components.map((code, index) => (
-          <div key={index} className="my-5 bg-blue-500 p-5">
-            <div
-              className="homeDiv"
-              dangerouslySetInnerHTML={{
-                __html: `<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><style>${code.css}</style></head><body>${code.html}</body>
-                <script>${code.js}</script></html>`,
-              }}
-              onClick={() => {
-               setHtml((prev)=>{
-                  return code.html
-
-               })
-               setCss((prev)=>{
-                return code.css
-                
-             })
-             setJs((prev)=>{
-              return code.js
-              
-           })
-           setIndexComponent((prev)=>{
-             return index
-           })
-
-              }}
-            >
-
+          <div key={index} className="my-5 p-5 ">
+            <div className="cursor-pointer">
+              <iframe
+                className="w-full"
+                srcDoc={`<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><style>${code.css}</style></head><body>${code.html}</body>
+              <script>${code.js}</script></html>`}
+                frameborder="0"
+                // style={{ pointerEvents: "none" }}
+              ></iframe>
             </div>
+            <div className="flex justify-between items-center my-4">
+              <div className="flex space-x-2">
+                <button
+                  onClick={() => moveElementUp(index)}
+                  className="bg-blue-500 text-white px-4 py-2 rounded"
+                >
+                  Move Up
+                </button>
+                <button
+                  onClick={() => moveElementDown(index)}
+                  className="bg-blue-500 text-white px-4 py-2 rounded"
+                >
+                  Move Down
+                </button>
+              </div>
+              <div className="flex space-x-2">
+                <button
+                  onClick={() => {
+                    setHtml(code.html);
+                    setCss(code.css);
+                    setJs(code.js);
+                    setShow("visible");
+                    setIndexComponent(index);
+                  }}
+                  className="bg-green-500 text-white px-4 py-2 rounded"
+                >
+                  Code
+                </button>
+                <button
+                  onClick={() => deleteElement(index)}
+                  className="bg-red-500 text-white px-4 py-2 rounded"
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+            <hr className="bg-white" />
           </div>
         ))}
-    
-    <div className="tab-button-container">
-          <Button title="HTML" onClick={() => {
-            onTabClick('html')
-            
-          }} />
-          <Button title="CSS" onClick={() => {
-            onTabClick('css')
-          }} />
-          <Button title="JavaScript" onClick={() => {
-            onTabClick('js')
-          }} />
-        </div>
-        
-        <div className="editor-container">
-          {
-            openedEditor === 'html' ? (
-              <Editor
-                value={html}
-                setEditorState={setHtmlCode}
-                
-              />
-            ) : openedEditor === 'css' ? (
-              <Editor
-              
-              value={css}
-              setEditorState={setCssCode}
-            />
-            ) : (
-              <Editor
-              value={js}
-              setEditorState={setJsCode}
-            />
-            )
-          }
-        </div>
 
-   
+        {components.length > 0 && (
+          <div>
+            <Draggable
+              axis="both"
+              // lastX=""
+              handle=".handle"
+              defaultPosition={{ x: 0, y: 0 }}
+              position={null}
+              grid={[25, 25]}
+              scale={1}
+            >
+              <div
+                className={`${show} handle bg-black shadow-sm p-5 rounded-md  shadow-gray-200 absolute top-[50%] right-10 w-1/2`}
+              >
+                <div className="flex cursor-grab active:cursor-grabbing  flex-col items-center text-center">
+                  <div className="flex  gap-x-1  text-gray-400 font-semibold text-2xl">
+                    <span>.</span>
+                    <span>.</span>
+                    <span>.</span>
+                  </div>
+                </div>
+                <div className={` flex flex-col`}>
+                  <div className="flex mb-5">
+                    <Button
+                      title="HTML"
+                      onClick={() => {
+                        onTabClick("html");
+                      }}
+                      isActive={openedEditor === "html"}
+                    />
+                    <Button
+                      title="CSS"
+                      onClick={() => {
+                        onTabClick("css");
+                      }}
+                      isActive={openedEditor === "css"}
+                    />
+                    <Button
+                      title="JavaScript"
+                      onClick={() => {
+                        onTabClick("js");
+                      }}
+                      isActive={openedEditor === "js"}
+                    />
+                  </div>
+
+                  <div className="editor-container">
+                    {openedEditor === "html" ? (
+                      <Editor value={html} setEditorState={setHtmlCode} />
+                    ) : openedEditor === "css" ? (
+                      <Editor value={css} setEditorState={setCssCode} />
+                    ) : (
+                      <Editor value={js} setEditorState={setJsCode} />
+                    )}
+                  </div>
+                </div>
+              </div>
+            </Draggable>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -30,33 +30,22 @@ export default function Home() {
   ]);
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
 
+  const changeCodes = (
+    newHtml: string,
+    newCss: string,
+    newJs: string,
+    index: number
+  ) => {
+    setCodes((prev) => {
+      console.log(index);
 
-  const changeCodes = (newHtml:string , newCss:string ,newJs:string,index:number)=>{
-    setCodes((prev)=>{
-      console.log(index)
-      
-      prev[index].html = newHtml
-      prev[index].css = newCss
-      prev[index].js = newJs
+      prev[index].html = newHtml;
+      prev[index].css = newCss;
+      prev[index].js = newJs;
 
-      return prev
-      
-    })
-  }
-
-
-  const changeCodes = (newHtml:string , newCss:string ,newJs:string,index:number)=>{
-    setCodes((prev)=>{
-      console.log(index)
-      
-      prev[index].html = newHtml
-      prev[index].css = newCss
-      prev[index].js = newJs
-
-      return prev
-      
-    })
-  }
+      return prev;
+    });
+  };
 
   const customModalStyles = {
     content: {
@@ -84,21 +73,6 @@ export default function Home() {
     };
   }, []);
 
-  // useEffect(() => {
-  //   axios
-  //     .post("api/hello")
-  //     .then(function (response: any) {
-  //       const { components } = response.data;
-  //       // setAllComps(components);
-  //       setAllComps(components);
-
-  //       // setCodes(components);
-  //     })
-  //     .catch(function (error: any) {
-  //       console.log(error);
-  //     });
-  // }, []);
-
   const fetchComponentsByCategory = (index: number) => {
     // Fetch components based on the selected category
     console.log(categories[index]);
@@ -125,6 +99,9 @@ export default function Home() {
     setIsModalOpen(false);
   };
 
+  const changeComponents = (code: any) => {
+    setCodes(code);
+  };
   // console.log(codes);
 
   return (
@@ -132,57 +109,77 @@ export default function Home() {
       <div className="w-full flex justify-center mt-5">
         <Login />
       </div>
-      <Component components={codes}  changeCodes ={changeCodes}/>
-      <div className="flex justify-center">
-        <button onClick={() => setIsModalOpen(true)} className="text-4xl">
-          {isModalOpen ? "✘" : "+"}
+      <div className="flex justify-center pt-5">
+        <button
+          onClick={() => setIsModalOpen(true)}
+          className="my-5 px-4 font-semibold text-lg bg-green-500 p-2 rounded-md text-center mx-2 cursor-pointer"
+        >
+          {isModalOpen ? "Close" : "Add"}
         </button>
       </div>
-      <Component components={codes} />
+      <Component
+        components={codes}
+        changeComponents={changeComponents}
+        changeCodes={changeCodes}
+      />
 
       <Modal
         isOpen={isModalOpen}
         onRequestClose={closeModal}
         style={customModalStyles}
       >
-        <div className="flex flex-wrap">
+        <div className="flex flex-wrap justify-center">
           {categories.map((category, index) => (
             <div
               key={index}
-              className="my-5 bg-blue-500 p-5 mx-2 cursor-pointer"
+              className="my-5 bg-blue-500 p-2 rounded-md text-center mx-2 cursor-pointer"
               onClick={() => openModal(index)}
             >
               {category}
             </div>
           ))}
         </div>
-
-        {/* Display components based on the selected category */}
+        <div className="absolute left-[90%] top-5 flex justify-center mt-5">
+          <button onClick={closeModal} className="p-2 rounded-md bg-red-500">
+            Close
+          </button>
+        </div>
         {selectedCategory && (
           <>
             <div className="text-white mb-3">
               Components under selected category:
             </div>
             {allComps.map((code, index) => (
-              <div key={index} className="my-5 bg-blue-500 p-5">
-                <div
-                  className="homeDiv"
-                  dangerouslySetInnerHTML={{
-                    __html: `<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><style>${code.css}</style></head><body>${code.html}</body><script>${code.js}</script></html>`,
-                  }}
-                  onClick={() => {
-                    setCodes((prevCode) => {
-                      return [...prevCode, code];
-                    });
-                  }}
-                ></div>
+              <div key={index} className="px-5">
+                <div>
+                  <iframe
+                    className="w-full"
+                    srcDoc={`<!DOCTYPE html><html><head><meta name="viewport" content="width=device-width, initial-scale=1"><style>${code.css}</style></head><body>${code.html}</body><script>${code.js}</script></html>`}
+                  ></iframe>
+                </div>
+                <div className="mb-5 flex justify-center">
+                  <button
+                    onClick={() => {
+                      setCodes((prevCode) => {
+                        const newCode: Code = {
+                          id: code.id,
+                          html: code.html,
+                          css: code.css,
+                          js: code.js,
+                        };
+                        return [...prevCode, newCode];
+                      });
+                    }}
+                    className="bg-blue-500 text-white px-4 py-2 rounded"
+                  >
+                    Choose
+                  </button>
+                </div>
+                <hr className="bg-white" />
               </div>
             ))}
           </>
         )}
-        <button onClick={closeModal} className="p-2 bg-red-500">
-          Close Button
-        </button>
       </Modal>
     </main>
   );
